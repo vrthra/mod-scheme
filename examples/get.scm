226d46a925c41b5803b8ade67a48ac186cf4fed3
blufox <blufox@gmail.com>
2007-12-13 10:39:15 +0000

    reformatting.
    M    examples/get.scm
    M    examples/imper_input_filter.scm
    M    examples/input_filter.scm
    M    examples/output_filter.scm
    M    examples/index.scm

--- examples/get.scm
+++ examples/get.scm
@@ -3,43 +3,43 @@
 (use apache:apr_buckets)
 (scheme:trace 0)
 (define (do_service req)
-      (display (apache:read_string 
-                           (apache:request_rec->args req)))
+  (display (apache:read_string 
+             (apache:request_rec->args req)))
   ;    (display  (apache:read_string 
   ;        (apache:read_post_data req)))
   (display (read_post_data req)))
 
 (function (for_each_brigade_loop bb)
-          (let loop_brigades ((bucket (apr:brigade_first bb)))
-            (if (not (eq? bucket (apr:brigade_sentinel bb)))
-              (begin
-                (if (apr:bucket_is_eos bucket)
-                  (return 1));;break / return from for_each
-                (if (not (apr:bucket_is_flush bucket))
-                  (let ((data (apache:new_ptr))(len 0))
-                    (apr:bucket_read bucket data len apr:block_read)
-                    (display (apache:nread_string data len))
-                    (loop_brigades (apr:bucket_next bucket)))
-                  (loop_brigades (apr:bucket_next bucket))))
-              0)));;if sentinel return seen_eos 0
+  (let loop_brigades ((bucket (apr:brigade_first bb)))
+    (if (not (eq? bucket (apr:brigade_sentinel bb)))
+      (begin
+        (if (apr:bucket_is_eos bucket)
+          (return 1));;break / return from for_each
+        (if (not (apr:bucket_is_flush bucket))
+          (let ((data (apache:new_ptr))(len 0))
+            (apr:bucket_read bucket data len apr:block_read)
+            (display (apache:nread_string data len))
+            (loop_brigades (apr:bucket_next bucket)))
+          (loop_brigades (apr:bucket_next bucket))))
+      0)));;if sentinel return seen_eos 0
 
 
 (function (read_post_data req)
-          (let* ((bb 
-                   (apr:brigade_create 
-                     (apache:request_rec->pool req) 
-                     (apache:request_rec->connection req) 
-                     (apache:conn_rec->bucket_alloc (apache:request_rec->connection req))))
-                 (seen_eos 0)
-                 (postdata (apache:new_ptr))
-                 (start 0)
-                 (huge_len 8192))
-            ;;while !seen_eos
-            (let while_not_seen_eos_loop ((rv))
-              (set! rv (ap:get_brigade (apache:request_rec->input_filters req) bb ap:mode_readbytes apr:block_read huge_len))
-              (if (not (= apr:success rv)) (return rv))
-              (set! seen_eos (for_each_brigade_loop bb))
-              (apr:brigade_cleanup bb)
-              (if (= 0 seen_eos) (while_not_seen_eos_loop)))));;while !seen_eos
+  (let* ((bb 
+           (apr:brigade_create 
+             (apache:request_rec->pool req) 
+             (apache:request_rec->connection req) 
+             (apache:conn_rec->bucket_alloc (apache:request_rec->connection req))))
+         (seen_eos 0)
+         (postdata (apache:new_ptr))
+         (start 0)
+         (huge_len 8192))
+    ;;while !seen_eos
+    (let while_not_seen_eos_loop ((rv))
+      (set! rv (ap:get_brigade (apache:request_rec->input_filters req) bb ap:mode_readbytes apr:block_read huge_len))
+      (if (not (= apr:success rv)) (return rv))
+      (set! seen_eos (for_each_brigade_loop bb))
+      (apr:brigade_cleanup bb)
+      (if (= 0 seen_eos) (while_not_seen_eos_loop)))));;while !seen_eos
 
 
--- examples/index.scm
+++ examples/index.scm
@@ -1,29 +1,27 @@
 (use apache:httpd)
 
 (define (do_service req)
-	(display (apache:read_string 
-		(apache:request_rec->args req)))
-	(display (myfunct))
-	)
+  (display (apache:read_string
+             (apache:request_rec->args req)))
+  (display (myfunct)))
 
 
 (function (myfunct)
-	(display "Testing")
-	(return "imperative return")
-	(display "Dont come here")
-)
-;(apache:request_rec->the_request req)
-		;(apache:request_rec->method req)
-		;(apache:request_rec->hostname req)
-		;(apache:request_rec->content_type req)
-		;(apache:request_rec->handler req)
-		;(apache:request_rec->content_encoding req)
-		;(apache:request_rec->uri req)
-		;
+  (display "Testing")
+  (return "imperative return")
+  (display "Dont come here"))
 
-;(apache:request_rec->args req)		
+;(apache:request_rec->the_request req)
+;(apache:request_rec->method req)
+;(apache:request_rec->hostname req)
+;(apache:request_rec->content_type req)
+;(apache:request_rec->handler req)
+;(apache:request_rec->content_encoding req)
+;(apache:request_rec->uri req)
+
+;(apache:request_rec->args req)
 ;(apache:request_rec->path_info req)
 
-
 ;(define (do_service req)
-;	(display (oblist)))
+;(display (oblist)))
+
