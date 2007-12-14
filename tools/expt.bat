12c21accedd357d6f8c9c957ad1a1037eadf9c3b
blufox <blufox@gmail.com>
2007-12-14 19:42:11 +0000

    D    tools/datatest.pl D    tools/expt.bat D    tools/regtest.pl

--- tools/expt.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-REM perl parseh.pl "C:/tools/serv/Apache2/include/expt"
-REM perl gencallback.pl -def
-REM perl gencallback.pl -dec
-REM perl sparseh.pl "C:/tools/serv/Apache2/include/expt"
-@echo on
-gen.bat "C:/tools/serv/Apache2/include"
--- tools/regtest.pl
+++
@@ -1,7 +0,0 @@
-print "\nEnter Regexp>>";
-$regexp = <STDIN>;
-chomp $regexp;
-print "[repl enter data] \n";
-while (<>) {
-    print "\nmatch [".$regexp."]" if /$regexp/;
-}
